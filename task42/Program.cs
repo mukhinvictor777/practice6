6a8f0e8e8b90803b68b93be481a686eee6cdf402
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-14 21:33:21 +0600

    task42 is complite

--- task42/Program.cs
+++ task42/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getNumberFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0 || result < 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0" || result < 0) Console.WriteLine($"Введите целое положительное число, вы вввели {userLine}"); else break;
+    }
+    return result;
+}
+
+int [] convertDecimalNumberToBinaryNumber (int decimalNumber)
+{
+    int countOfDigit = 1;
+    int bufferNumber = decimalNumber;
+    while (bufferNumber / 2 > 0)
+    {
+        countOfDigit++;
+        bufferNumber /= 2;
+    }
+    int [] convertedArray = new int[countOfDigit];
+    for (int i = 0; i < convertedArray.Length; i++)
+    {
+        convertedArray[countOfDigit-i-1] = decimalNumber % 2;
+        decimalNumber /= 2;
+    }
+    return convertedArray;
+}
+
+void printConvertedNumber(int [] convertedArray, int numberInTen)
+{
+    Console.Write($"Число {numberInTen} в десятичной системе счисления равно ");
+    for (int i = 0; i < convertedArray.Length; i++)
+    {
+        Console.Write(convertedArray[i]);
+    }
+    Console.Write(" в двоичной системе счисления");
+}
+
+int numberInTen = getNumberFromUser($"Введите положительное число: ");
+int [] convertedArray = convertDecimalNumberToBinaryNumber(numberInTen);
+printConvertedNumber(convertedArray, numberInTen);
