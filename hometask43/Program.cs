c06920db7ed4f17eb9587b40d4fa92ad96505214
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-15 11:13:56 +0600

    hometask 43 is complite

--- hometask43/Program.cs
+++ hometask43/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double getDoubleNumberFromUser(string userInformation)
+{
+    double  result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        double.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0") Console.WriteLine($"Введите число, вы вввели {userLine}"); else break;
+    }
+    return result;
+}
+
+double intersectionX (double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);   
+    return x;
+}
+
+double intersectionY (double k1, double b1, double x)
+{   
+    double y = k1 * x + b1;
+    return y;
+}
+
+Console.Write("Введите значение b1 для уравнения (y = k1 * x + b1) ");
+double userB1 = getDoubleNumberFromUser("b1 = ");
+Console.Write("Введите значение k1 для уравнения (y = k1 * x + b1) ");
+double userK1 = getDoubleNumberFromUser("k1 = ");
+Console.Write("Введите значение b2 для уравнения (y = k2 * x + b2) ");
+double userB2 = getDoubleNumberFromUser("b2 = ");
+Console.Write("Введите значение k2 для уравнения (y = k2 * x + b2) ");
+double userK2 = getDoubleNumberFromUser("k2 = ");
+Console.WriteLine("Для прямых заданных уравнениями:");
+Console.WriteLine($"y = {userK1} * x + {userB1}");
+Console.WriteLine($"y = {userK2} * x + {userB2}");
+double coordinateX = intersectionX(userB1, userK1, userB2, userK2);
+double coordinateY = intersectionY(userK1, userB1, coordinateX);
+Console.WriteLine($"Пересечением является точка A [{coordinateX};{coordinateY}]");
--- task42/Program.cs
+++ task42/Program.cs
@@ -1,44 +1,55 @@
 ﻿int getNumberFromUser(string userInformation)
 {
     int  result = 0;
-    while (result == 0 || result < 0)
+    while (result == 0)
     {
         Console.Write(userInformation);
         string userLine = Console.ReadLine();
         int.TryParse(userLine, out result);
-        if (result == 0 && userLine != "0" || result < 0) Console.WriteLine($"Введите целое положительное число, вы вввели {userLine}"); else break;
+        if (result == 0 && userLine != "0") Console.WriteLine($"Введите целое число, вы вввели {userLine}"); else break;
     }
     return result;
 }
 
-int [] convertDecimalNumberToBinaryNumber (int decimalNumber)
+void printArray(int[] incomingArray)
 {
-    int countOfDigit = 1;
-    int bufferNumber = decimalNumber;
-    while (bufferNumber / 2 > 0)
+    for (int i = 0; i < incomingArray.Length; i++)
     {
-        countOfDigit++;
-        bufferNumber /= 2;
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }       
     }
-    int [] convertedArray = new int[countOfDigit];
-    for (int i = 0; i < convertedArray.Length; i++)
-    {
-        convertedArray[countOfDigit-i-1] = decimalNumber % 2;
-        decimalNumber /= 2;
-    }
-    return convertedArray;
+    Console.WriteLine();
 }
 
-void printConvertedNumber(int [] convertedArray, int numberInTen)
+int [] getIntegerArrayFromUser()
 {
-    Console.Write($"Число {numberInTen} в десятичной системе счисления равно ");
-    for (int i = 0; i < convertedArray.Length; i++)
+    int userArrayLenght = getNumberFromUser ("Какое количество целых чисел Вы хотите ввести?: ");
+    int [] userArray = new int[userArrayLenght];
+    for (int i = 0; i < userArrayLenght; i++)
     {
-        Console.Write(convertedArray[i]);
+        userArray[i] = getNumberFromUser ($"Введите {i+1} число: ");
     }
-    Console.Write(" в двоичной системе счисления");
+    Console.WriteLine("Вы ввели след набор чисел:");
+    printArray(userArray);
+    return userArray;
 }
 
-int numberInTen = getNumberFromUser($"Введите положительное число: ");
-int [] convertedArray = convertDecimalNumberToBinaryNumber(numberInTen);
-printConvertedNumber(convertedArray, numberInTen);
+int countOfPositiveNumbers(int [] userArray)
+{
+    int count = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int [] userArray = getIntegerArrayFromUser();
+int countOfPositive = countOfPositiveNumbers(userArray);
+Console.WriteLine($"Количество положительных чисел в заданом массиве равно {countOfPositive}");
