5f5a60f5c520b89e1980d3bcf9dc518ddf682055
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-24 22:40:52 +0600

    Решил часть задачи с подмножестами

---
+++ functions/Program.cs
@@ -0,0 +1,150 @@
+﻿int getLenghtFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0 || result < 1)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 || result < 1) Console.WriteLine($"Введите целое число больше нуля, вы вввели {userLine}"); else break;
+    }
+    return result;
+}
+
+int getNumberFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0") Console.WriteLine($"Введите целое, вы вввели {userLine}"); else break;
+    }
+    return result;
+}
+
+
+int [] getRandomArray(int arrayLenght, int startPoint, int endPoint)
+{
+    int [] randomArray = new int [arrayLenght];
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        randomArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return randomArray;
+}
+
+
+int [] createCopyArray(int[] incomingArray)
+{
+    int [] copiedArray = new int [incomingArray.Length];
+    for (int i = 0; i < copiedArray.Length; i++)
+    {
+        copiedArray[i] = incomingArray[i];
+    }
+    return copiedArray;
+}  
+
+int [] RemovedElementInArray(int[] incomingArray, int index)
+    {
+        if (incomingArray.Length == index)
+        {
+            Array.Resize(ref incomingArray, incomingArray.Length - 1);
+        }
+        else
+        {
+            for (int i = index; i < incomingArray.Length - 1; i++)
+            {
+                incomingArray[i] = incomingArray[i+1];
+            }
+            Array.Resize(ref incomingArray, incomingArray.Length - 1);
+        }
+        return incomingArray;
+    }
+
+int [] addElementInArray(int[] incomingArray, int index, int element)
+    {
+        Array.Resize(ref incomingArray, incomingArray.Length + 1);
+            for (int i = index; i < incomingArray.Length - 1; i++)
+            {
+                incomingArray[i+1] = incomingArray[i];
+            }
+            incomingArray[index] = element;
+    return incomingArray;
+    }
+
+
+           
+
+void checkPlentyFromRandomArray (int [] RandomArray, int plentySum)
+{
+    int [] plenty = new int [RandomArray.Length];
+    int sumOfPositive = sumOfPositiveNumbers (RandomArray);
+    int sumOfNegative = sumOfNegativeNumbers (RandomArray);
+    int sum = sumOfNumbersInArray(RandomArray);
+    int tempSum = sum;
+    int buffer = 0;
+    if (sum >= sumOfPositive)
+    {
+        Console.Write("В множистве ");
+        printArray(RandomArray);
+        Console.WriteLine($" не существует подмножества с суммой всех элементов равной {plentySum}");
+    }
+    else if (sum <= sumOfNegative)
+        {
+            Console.Write("В множистве ");
+            printArray(RandomArray);
+            Console.WriteLine($" не существует подмножества с суммой всех элементов равной {plentySum}");
+        }
+        else
+        {
+            if (plentySum == 0)
+            {
+                Console.Write("В множистве ");
+                printArray(RandomArray);
+                Console.WriteLine(" существует пустое подмножество, где нет ни одного элемента");
+                Console.WriteLine();
+            }
+            if (sum == plentySum)
+            {
+                plenty = createCopyArray(RandomArray);
+                Console.Write("В множистве ");
+                printArray(RandomArray);
+                Console.WriteLine($" существует подмножество с суммой всех элементов равной {plentySum}");
+                printArray(plenty);
+                Console.WriteLine();
+            }
+            
+
+
+            for (int i = 0; i < RandomArray.Length; i++)
+            {
+                for (int j = 0; j < plenty.Length; j++)
+                {
+                    if (plentySum == tempSum - RandomArray[j]) // проверяем для каждого элемента масиива выполняется ли равенство, при котором, если убрать любой другой элемент,
+                                                               // будет ли сумма оставшихся елементов равняться сумме искомого множества, другими словами, перебираем все
+                                                               // возможные подмножества и проверяем есть ли среди них такие, сумма которых равна заданному числу. 
+                                                               
+                    {
+                       buffer = RandomArray[j];
+                       plenty = RemovedElementInArray(plenty, j);   //удаляем элемент из массива, без которого сумма оставшихся элементов в массиве равна заданному числу
+                       Console.WriteLine($" существует подмножество с суммой всех элементов равной {plentySum}"); 
+                       printArray(plenty); // выводим на экран множество, которое соответствует условию
+                       Console.WriteLine();
+                       plenty = addElementInArray(plenty, j, buffer); // возвращаем удаленный элемент на место для дальнешего поиска подходящих подмножеств в рамках заданного цикла.
+                    }
+                }
+                // for (int k = 0; k < i; k++)
+                //tempSum = tempSum - RandomArray[i];
+            }
+        }    
+}
+
+// int arrayLenght = getLenghtFromUser ("Введите длину массива целых чисел: ");
+// int startPoint = getNumberFromUser ("Задайте минимально возможное значение в массиве: ");
+// int endPoint = getNumberFromUser ("Задайте максимально возможное значение в массиве: ");
+// int [] randomArray = getRandomArray(arrayLenght, startPoint, endPoint);
+// printArray(randomArray);
+// int plentySum = getNumberFromUser ("Введите сумму элементов для определения подмножества ");
+// checkPlentyFromRandomArray(randomArray, plentySum);
--- hometaskExtra/Program.cs
+++ hometaskExtra/Program.cs
@@ -1,2 +1,205 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void printArray(int[] incomingArray)
+{
+    Console.Write(" [ ");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(" ");
+        }       
+    }
+    Console.Write(" ] ");
+}
+
+int sumOfPositiveNumbers(int [] userArray)
+{
+    int sum = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i] > 0)
+        {
+            sum = sum + userArray[i];
+        }
+    }
+    return sum;
+}
+
+int sumOfNegativeNumbers(int [] userArray)
+{
+    int sum = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i] < 0)
+        {
+            sum = sum + userArray[i];
+        }
+    }
+    return sum;
+}
+
+int sumOfNumbersInArray(int [] userArray, int startIndex, int EndIndex)
+{
+    int sum = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        {
+            sum = sum + userArray[i];
+        }
+    }
+    return sum;
+}
+
+int[] sortArray(int[] incomingArray)
+{
+    int buffer = 0;
+    for (int i = 0; i < incomingArray.Length - 1; i++)
+    {
+        for (int j = i; j < incomingArray.Length; j++)
+        {
+            if (incomingArray[j] < incomingArray[i])
+            {
+                buffer = incomingArray[i];
+                incomingArray[i] = incomingArray[j];
+                incomingArray[j] = buffer;
+            }
+        }
+    }
+    return incomingArray;
+}
+
+int [] RemovedElementInArray(int[] incomingArray, int index)
+    {
+        if (incomingArray.Length - 1 == index)
+        {
+            Array.Resize(ref incomingArray, incomingArray.Length - 1);
+        }
+        else
+        {
+            for (int i = index; i < incomingArray.Length - 1; i++)
+            {
+                incomingArray[i] = incomingArray[i+1];
+            }
+            Array.Resize(ref incomingArray, incomingArray.Length - 1);
+        }
+        return incomingArray;
+    }
+
+void checkPlentyFromArray(int [] userArray, int plentySum)
+{
+    int sumOfPositive = sumOfPositiveNumbers (userArray);
+    int sumOfNegative = sumOfNegativeNumbers (userArray);
+    int sum = sumOfNumbersInArray(userArray, 0, userArray.Length);
+    int tempLenght = userArray.Length;
+    int [] plenty = userArray;
+    int positive = 1;
+    int negative = 1;
+    Console.WriteLine();
+    if (plentySum > sumOfPositive)
+    {
+        Console.Write("В множистве ");
+        printArray(userArray);
+        Console.WriteLine($" не существует подмножества с суммой всех элементов равной {plentySum}");
+        Console.WriteLine();
+    }
+        else if (plentySum < sumOfNegative)
+        {
+            Console.Write("В множистве ");
+            printArray(userArray);
+            Console.WriteLine($" не существует подмножества с суммой всех элементов равной {plentySum}");
+            Console.WriteLine();
+        }
+        else
+        {
+            if (plentySum == 0)
+            {
+                Console.Write("В множистве ");
+                printArray(userArray);
+                Console.WriteLine(" существует пустое подмножество, где нет ни одного элемента");
+                Console.WriteLine();
+                for (int i = 0; i < tempLenght; i++)
+                {
+                    if (plenty[i] == 0)
+                    {
+                        Console.Write("В множистве ");
+                        printArray(userArray);
+                        Console.WriteLine(" существует пустое подмножество, состоящее из [ 0 ]");
+                        Console.WriteLine();
+                        plenty = RemovedElementInArray(plenty, i);
+                        tempLenght -= 1;
+                    }
+                }
+            }
+            if (sum == plentySum)
+            {
+                Console.Write("В множистве ");
+                printArray(userArray);
+                Console.WriteLine($" существует подмножество с суммой всех элементов равной {plentySum}");
+                printArray(userArray);
+                Console.WriteLine();
+            }
+            for (int i = 0; i < tempLenght; i++)
+            {
+                if (plenty[i] == 0)
+                {
+                    plenty = RemovedElementInArray(plenty, i);
+                    tempLenght -= 1; 
+                }
+            }
+            plenty = sortArray(plenty);
+            printArray(plenty);
+            Console.WriteLine();
+            tempLenght = plenty.Length;
+            
+            for (int i = 0; i < plenty.Length; i++)
+            {
+                if (plenty[i] < 0)
+                {
+                    positive = 0;
+                    break;
+                }
+            }
+            if (positive == 1)
+            {
+                for (int i = tempLenght - 1; i >= 0; i--)
+                {
+                    if (plentySum < plenty[i])
+                    {
+                        plenty = RemovedElementInArray(plenty, i);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < tempLenght; i++)
+                {
+                    if (plenty[i] > 0)
+                    {
+                        negative = 0;
+                        break;
+                    }
+                }
+                if (negative == 1)
+                {
+                    for (int i = 0; i < tempLenght; i++)
+                    {
+                        if (plenty[i] < plentySum)
+                        {
+                            plenty = RemovedElementInArray(plenty, i);
+                            tempLenght--;
+                        }
+                    }
+                }
+            }
+                      
+            tempLenght = plenty.Length;
+            printArray(plenty);
+            Console.WriteLine();
+        }
+}
+
+int [] userArray = new int [8] {-1, 0, -8, -5, -7, -5, -3, -15};
+printArray(userArray);
+checkPlentyFromArray(userArray, -10);
+
+
